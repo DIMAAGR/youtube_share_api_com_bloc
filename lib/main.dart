168efc016b8bdca8ee803b6837e590e3fadeb60e
Italo Souza <47384565+DIMAAGR@users.noreply.github.com>
2022-11-14 23:19:31 -0300

    configurando projeto

---
+++ lib/main.dart
@@ -0,0 +1,4 @@
+import 'package:flutter/cupertino.dart';
+import 'package:youtube_share_api_com_bloc/src/config/main/main_app.dart';
+
+void main(List<String> args) => runApp(const MainApp());
---
+++ lib/src/config/main/main_app.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:youtube_share_api_com_bloc/src/home/home_view.dart';
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: HomeView(),
+      title: 'Youtube App (FlutterTube)',
+    );
+  }
+}
