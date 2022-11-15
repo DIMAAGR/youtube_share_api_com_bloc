4b090eef90f05e6177afdaf7c4beed1e69c7b1af
Italo Souza <47384565+DIMAAGR@users.noreply.github.com>
2022-11-14 23:19:38 -0300

    adicionando api_key

---
+++ lib/src/config/constants.dart
@@ -0,0 +1,12 @@
+class Const {
+  Const._();
+
+  static const String apiKey = 'AIzaSyBGjjSBzdrs09l3A6lHmvF9Rc3Qx4ehN10';
+}
+
+/*
+"https://www.googleapis.com/youtube/v3/search?part=snippet&q=$search&type=video&key=$API_KEY&maxResults=10"
+"https://www.googleapis.com/youtube/v3/search?part=snippet&q=$_search&type=video&key=$API_KEY&maxResults=10&pageToken=$_nextToken"
+"http://suggestqueries.google.com/complete/search?hl=en&ds=yt&client=youtube&hjson=t&cp=1&q=$search&format=5&alt=json"
+
+ */
